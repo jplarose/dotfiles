53410fb5f7ff7b35f551ca0dceab7798069905bd
Justin LaRose <justinplarose@gmail.com>
2026-04-14 14:57:51 -0400

    Adding a UI skill into Claude and Codex for better UI dev implementations.

--- nvim/.config/nvim/lua/plugins/referencer.lua
+++ nvim/.config/nvim/lua/plugins/referencer.lua
@@ -10,7 +10,7 @@ return {
       show_no_reference = true,
       kinds = { 5, 6, 8, 12, 13, 14, 23 },
       hl_group = 'Comment',
-      color = '#FFA500',
+      color = '#a6a6a6',
       virt_text_pos = 'eol',
     }
   end,
